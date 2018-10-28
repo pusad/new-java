6c140e396de41459e51146a4610418e312d53420
lenovo <lenovo@DESKTOP-IO91LHB>
2018-10-28 11:26:03 -0700

    nn

--- SIMPLE CLASS/src/com/demo/aa.java
+++ SIMPLE CLASS/src/com/demo/aa.java
@@ -19,6 +19,9 @@ public class aa implements Serializable
 		 System.out.println("umesh deshmukh");
 		 System.out.println();
 		 
+		 
+		 
+		 
 	  aa t1=new aa();
 	  t1.a=100;
 	  t1.b=200000000;
